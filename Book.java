32aaeb62fa38a66c34f92ddcd7b63bb1b7dd6485
Philippe Lizotte <55700508+PhilLizotte@users.noreply.github.com>
2026-02-21 11:09:56 -0500

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,233 @@
+import java.io.Serializable;
+import Exc.*;
+
+// -----------------------------------------------------
+// Assignment 2
+// Part 1
+// Written by: Philippe Lizotte - 40261140
+// ----------------------------------------------------- 
+
+// This is a class with 6 properties, each with getters and setters.
+// - It also keeps track of how many books have been created in all.
+// - Two books are considered equal if they have the same ISBN and price.
+// - Also has a basic toString and equals override.
+
+// *** TO-DO: Proper javadoc for class/author(s)/whatever else ***
+/**
+* A Book class used mostly for storing book information.
+* 
+* @author Philippe Lizotte
+*/
+class Book implements Serializable {
+    private String title;
+	private String authors;
+	private double price;
+	private String ISBN;
+	private String genre;
+	private int year;
+	
+	private static int createdBooks = 0;
+	
+	/**
+	* Simple constructor that assigns values to all properties.
+	*/
+	public Book(String title, String authors, double price, String ISBN, String genre, int year)
+	throws BadPriceException, BadIsbn10Exception, BadIsbn13Exception, BadYearException {
+		setTitle(title);
+		setAuthors(authors);
+		setPrice(price);
+		setISBN(ISBN);
+		setGenre(genre);
+		setYear(year);
+		
+		createdBooks++;
+	}
+	
+	/**
+	* Simple getter
+	*
+	* @return       title property
+	*/
+	public String getTitle() {
+		return title;
+	}
+	
+	/**
+	* Simple setter
+	*
+	* @param       newTitle new title property
+	*/
+	public void setTitle(String newTitle) {
+		this.title = newTitle;
+	}
+	
+	/**
+	* Simple getter
+	*
+	* @return       authors property
+	*/
+	public String getAuthors() {
+		return authors;
+	}
+	
+	/**
+	* Simple setter
+	*
+	* @param       newAuthors new authors property
+	*/
+	public void setAuthors(String newAuthors) {
+		this.authors = newAuthors;
+	}
+	
+	/**
+	* Simple getter
+	*
+	* @return       ISBN property
+	*/
+	public String getISBN() {
+		return ISBN;
+	}
+	
+	/**
+	* Setter that validates ISBN entries
+	*
+	* @param       newISBN new ISBN property
+	* @throws      BadIsbn10Exception
+	* @throws      BadIsbn13Exception
+	*/
+	public void setISBN(String newISBN) throws BadIsbn10Exception, BadIsbn13Exception {
+		
+		String f_msg = "Incorrect 1%d-digit ISBN format! \"%s\" does not follow convention.";
+		
+		if (newISBN.length() == 10) {
+			int sum = 0;
+			for (int i = 0; i < 10; i++) {
+				sum += (10 - (i + 1)) * Integer.valueOf(newISBN.charAt(i));
+			}
+			
+			if (sum % 11 != 0) {
+				throw new BadIsbn10Exception(String.format(f_msg, 0, newISBN));
+			}
+		} else if (newISBN.length() == 13) {
+			int sum = 0;
+			for (int i = 0; i < 13; i++) {
+				int scalar = i % 2 == 0 ? 1 : 3;
+				sum += scalar * Integer.valueOf(newISBN.charAt(i));
+			}
+			
+			if (sum % 10 != 0) {
+				throw new BadIsbn13Exception(String.format(f_msg, 3, newISBN));
+			}
+			
+		} else {
+			// If length isn't 10 or 13, just throw a BadIsbn10Exception
+			throw new BadIsbn10Exception("Incorrect number of digits in " + newISBN + ".");
+		}
+		
+		this.ISBN = newISBN;
+	}
+	
+	/**
+	* Simple getter
+	*
+	* @return       price property
+	*/
+	public double getPrice() {
+		return price;
+	}
+	
+	/**
+	* Price setter that checks for valid price format.
+	*
+	* @param       newPrice new price property
+	* @throws      BadPriceException
+	*/
+	public void setPrice(double newPrice) throws BadPriceException {
+		if (newPrice < 0) {
+			String f_msg = "Incorrect price format! %.2f must not be nonnegative.";
+			throw new BadPriceException(String.format(f_msg, newPrice));
+		}
+		this.price = newPrice;
+	}
+	
+	/**
+	* Simple getter
+	*
+	* @return       genre property
+	*/
+	public String getGenre() {
+		return genre;
+	}
+	
+	/**
+	* Simple setter
+	*
+	* @param       newGenre new genre property
+	*/
+	public void setGenre(String newGenre) {
+		this.genre = newGenre;
+	}
+	
+	/**
+	* Simple getter
+	*
+	* @return       year property
+	*/
+	public int getYear() {
+		return year;
+	}
+	
+	/**
+	* Year setter that only accepts years in the inclusive range [1995, 2024]
+	*
+	* @param       newYear new year property
+	* @throws      BadYearException
+	*/
+	public void setYear(int newYear) throws BadYearException {
+		if (newYear < 1995) {
+			String f_msg = "Incorrect year format! %d must not be less than 1995 or greater than 2024.";
+			throw new BadYearException(String.format(f_msg, newYear));
+		}
+		if (newYear > 2024) {
+			String f_msg = "Incorrect year format! %d must not be less than 1995 or greater than 2024.";
+			throw new BadYearException(String.format(f_msg, newYear));
+		}
+		this.year = newYear;
+	}
+	
+	/**
+	* Static function that returns the number of books created using a
+	* value that increments whenever the constructor is called.
+	*
+	* @return      The number of books created up to this point.
+	*/
+	public static int findNumberOfCreatedBooks() {
+		return createdBooks;
+	}
+	
+	/**
+	* Comparison function that returns true if the ISBN and price of both books are equal.
+	*
+	* @param       obj external Object (ideally a Book)
+	* @return      boolean value, true if the books are equal.
+	*/
+	@Override
+	public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || obj.getClass() != Book.class) return false;
+		
+		Book that = (Book) obj;
+		return this.ISBN == that.ISBN && this.price == that.price;
+	}
+	
+	/**
+	* Simple toString override
+	*
+	* @return      A list of this object's properties, formatted for printing.
+	*/
+    @Override
+    public String toString() {
+		String f_string = "Author: %s\nTitle: %s\nPrice: %.2f\nISBN: %s\nGenre: %s\nYear: %d";
+        return String.format(f_string, getAuthors(), getTitle(), getPrice(), getISBN(), getGenre(), getYear());
+    }
+}
